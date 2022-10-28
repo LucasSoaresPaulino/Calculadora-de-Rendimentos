e460ce28c6b23b5aa0683295675b61b8617b1ae1
LucasSoaresPaulino <lucassoarespaulino1010@gmail.com>
2022-10-28 16:19:32 -0300

    Subindo Projeto Calculadora de Rendimentos

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace Calculadora_de_rendimentos
+{
+    partial class design
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(design));
+            this.aporteTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.mesesLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.prazoTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.taxaTextBox = new System.Windows.Forms.TextBox();
+            this.calcular = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.valorTotalTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lucroTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // aporteTextBox
+            // 
+            this.aporteTextBox.Location = new System.Drawing.Point(55, 61);
+            this.aporteTextBox.Name = "aporteTextBox";
+            this.aporteTextBox.Size = new System.Drawing.Size(108, 26);
+            this.aporteTextBox.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.aporteTextBox);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 108);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "APORTE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "R$";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.mesesLabel);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.prazoTextBox);
+            this.groupBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(321, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(266, 108);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PRAZO";
+            // 
+            // mesesLabel
+            // 
+            this.mesesLabel.AutoSize = true;
+            this.mesesLabel.Location = new System.Drawing.Point(179, 64);
+            this.mesesLabel.Name = "mesesLabel";
+            this.mesesLabel.Size = new System.Drawing.Size(59, 19);
+            this.mesesLabel.TabIndex = 7;
+            this.mesesLabel.Text = "meses";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "R$";
+            // 
+            // prazoTextBox
+            // 
+            this.prazoTextBox.Location = new System.Drawing.Point(51, 62);
+            this.prazoTextBox.Name = "prazoTextBox";
+            this.prazoTextBox.Size = new System.Drawing.Size(108, 26);
+            this.prazoTextBox.TabIndex = 3;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.taxaTextBox);
+            this.groupBox4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(625, 29);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(266, 108);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "TAXA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(179, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 19);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "%a.m.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "R$";
+            // 
+            // taxaTextBox
+            // 
+            this.taxaTextBox.Location = new System.Drawing.Point(51, 61);
+            this.taxaTextBox.Name = "taxaTextBox";
+            this.taxaTextBox.Size = new System.Drawing.Size(108, 26);
+            this.taxaTextBox.TabIndex = 3;
+            // 
+            // calcular
+            // 
+            this.calcular.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcular.Location = new System.Drawing.Point(111, 207);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(696, 69);
+            this.calcular.TabIndex = 7;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 341);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(310, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Valor total ao final do periodo ";
+            // 
+            // valorTotalTextBox
+            // 
+            this.valorTotalTextBox.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
+            this.valorTotalTextBox.Location = new System.Drawing.Point(399, 338);
+            this.valorTotalTextBox.Name = "valorTotalTextBox";
+            this.valorTotalTextBox.Size = new System.Drawing.Size(149, 32);
+            this.valorTotalTextBox.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(344, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "R$";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(269, 404);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 24);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Lucro";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(344, 404);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 24);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "R$";
+            // 
+            // lucroTextBox
+            // 
+            this.lucroTextBox.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
+            this.lucroTextBox.Location = new System.Drawing.Point(399, 396);
+            this.lucroTextBox.Name = "lucroTextBox";
+            this.lucroTextBox.Size = new System.Drawing.Size(149, 32);
+            this.lucroTextBox.TabIndex = 12;
+            // 
+            // design
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Calculadora_de_rendimentos.Properties.Resources.calcular;
+            this.ClientSize = new System.Drawing.Size(903, 483);
+            this.Controls.Add(this.lucroTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.valorTotalTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "design";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Rendimentos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox aporteTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox prazoTextBox;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox taxaTextBox;
+        private System.Windows.Forms.Label mesesLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox valorTotalTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox lucroTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_de_rendimentos
+{
+    public partial class design : Form
+    {
+        public design()
+        {
+            InitializeComponent();
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+        // o metodo de variaveis em decimal, permite q o usuario utilize vergulas, numero quebrados, etc;   
+            decimal aporte;
+            decimal prazo;
+            decimal taxa;
+            decimal valorfinal;
+            
+        // variaveis apenas armazena texto, portanto precisa ser utilizado a função convert.Todecimal(); 
+            aporte = Convert.ToDecimal(aporteTextBox.Text);
+            prazo = Convert.ToDecimal (prazoTextBox.Text);
+            taxa = Convert.ToDecimal (taxaTextBox.Text);
+            
+            taxa = taxa / 100;  // taxa é igual o numero digitado pelo usuario, dividido por 100.
+
+            valorfinal = aporte;
+
+        //   a variavel chamada de "i" vai ficar executando repetidamente o nosso calculo, com base no numero de meses que o usuario colocou na nossa variavel chamada "prazo".
+            for (int i = 0; i<prazo; i++)
+            {
+                valorfinal = valorfinal + (valorfinal * taxa);
+        //      valorfinal =    100     + (  100      * 0,01)
+        //      valorfinal =    100     +            1       =    101  
+            }
+
+        // string é um tipo de variavel. o simbolo "[]" significa Array. que basicamente permite ser armazenada muitas informações dentro de uma variavel, e tambem permite uma organização melhor da mesma.
+            string[] vf;
+
+            vf = Convert.ToString(valorfinal).Split(','); // o "split" vc pode cortar a variavel, ou seja; por exemplo vc pode cortar um numero ou um nome que nao quer utilizar
+           valorTotalTextBox.Text = "R$" + vf[0] + "," + vf[1].Substring(0,2);
+            // É necessario o ".text" para poder dizer q o resultado aparecera em formato de texto dentro da TextBox. O "Convert.ToString" é necessario para transformar os numeros declarados na varialvel valorfinal em texto dentro da TextBox    
+
+            decimal lucro = valorfinal - aporte;
+
+            lucroTextBox.Text = Convert.ToString(lucro);
+
+            string[] lf;
+
+            lf = Convert.ToString(lucro).Split(',');
+            lucroTextBox.Text = "R$" + lf[0] + "," + lf[1].Substring(0, 2);
+
+        }
+    }
+}
